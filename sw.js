33f842615f4189e789fe487437054643b7d00eea
chrstianfel <chrstianfelll@gmail.com>
2026-06-30 15:07:53 +0700

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,29 @@
+const CACHE = 'target-tracker-v1';
+const ASSETS = ['./', './index.html', './manifest.json'];
+
+self.addEventListener('install', (e) => {
+  self.skipWaiting();
+  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)));
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((cached) => cached || fetch(e.request).catch(() => cached))
+  );
+});
+
+// Tampilkan notifikasi saat di-trigger dari halaman (lewat postMessage)
+self.addEventListener('message', (e) => {
+  if (e.data && e.data.type === 'SHOW_NOTIF') {
+    self.registration.showNotification(e.data.title, e.data.options);
+  }
+});
